18dc0eba97f7be76d53641175a02e908973d9f92
Yamayamaaya <yugo139013@gmail.com>
2024-06-25 03:32:31 +0900

    ドキュメント更新

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,19 +6,22 @@ class MyDocument extends Document {
       <Html lang="ja" prefix="og: http://ogp.me/ns#">
         <Head>
           {/* ページの説明 */}
-          <meta name="description" content="ここにページの説明を入力" />
+          <meta name="description" content="タイピング王に俺はなる！" />
           {/* OGP (Open Graph Protocol) タイトル */}
-          <meta property="og:title" content="ここにサイトのタイトルを入力" />
+          <meta property="og:title" content="TypingKing" />
           {/* OGP 説明 */}
-          <meta property="og:description" content="ここにサイトの説明を入力" />
+          <meta property="og:description" content="タイピング王に俺はなる！" />
           {/* OGP タイプ */}
           <meta property="og:type" content="website" />
           {/* OGP URL */}
-          <meta property="og:url" content="ここにサイトのURLを入力" />
+          <meta
+            property="og:url"
+            content="https://typingking-3d19d.web.app/signin"
+          />
           {/* OGP 画像 */}
-          <meta property="og:image" content="ここにOGP画像のパスを入力" />
+          <meta property="og:image" content="/favicon.ico" />
           {/* OGP サイト名 */}
-          <meta property="og:site_name" content="ここにサイト名を入力" />
+          <meta property="og:site_name" content="TypingKing" />
           {/* OGP ロケール */}
           <meta property="og:locale" content="ja_JP" />
         </Head>
